2402b6a7ee378c4acd9cf0af28f1bebca1ac1a82
플렝미 <forflame0@gmail.com>
2020-05-12 18:32:54 +0900

    no message

--- routes/test.js
+++ routes/test.js
@@ -34,17 +34,52 @@ router.get('/wpadd', (req,res,next) => {
   });
 });
 router.get('/fixwp', (req,res,next) => {
+  var Seachname = req.query.Seachname
+  var QueryString = "select * from aquafeq.aquafwp where wpname = Seachname"
   res.render ('fixwp', {
-    title:'AAF 장비'
+
+    title:'AAF 장비',
+    data:response.rows
+
   });
 });
 
 router.post('/fixwp', (req,res,next) => {
-  var Select_id = req.body.form_id
-  var QueryString = "select * from aquafeq.aquafwp where wpid = Select_id ;"
-  client.query(QueryString, (err, response) => {
+  var Select_name = req.query.Seachname
+  var Wpgrade = req.body.wpgrade;
+    if (Wpgrade !== '') {
+      Wpgrade = Wpgrade.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wplimit = req.body.wplimit;
+    if (Wplimit == '') {
+        Wplimit = null
+    }
+  var Wpsocket = req.body.wpsocket;
+  var Wpether = req.body.wpether;
+  var Wpstats = req.body.wpstats;
+    if (Wpstats !== '') {
+      Wpstats = Wpstats.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wpproperty = req.body.wpproperty;
+    if (Wpproperty !== '') {
+      Wpproperty = Wpproperty.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wpfeat = req.body.wpfeat;
+    if (Wpfeat !== '') {
+      Wpfeat = Wpfeat.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wpcustom = req.body.wpcustom;
+    if (Wpcustom !== ''){
+      Wpcustom = Wpcustom.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wpup = req.body.wpup;
+    if (Wpup !== '') {
+      Wpup = Wpup.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var QueryString = "UPDATE aquafeq.aquafwp SET wpgrade = $1, wplimit = $2, wpsocket = $3, wpether = $4, wpstats = $5, wpproperty = $6, wpfeat = $7, wpcustom = $8, wpup = $9  WHERE aquafeq.aquafwp.wpname = Select_name "
+  client.query(QueryString, [Wpgrade, Wplimit, Wpsocket, Wpether, Wpstats, Wpproperty, Wpfeat, Wpcustom, Wpup], (err, response) => {
 
-    res.render ('fixedwp', {
+    res.render ('fixwp', {
       title:response.rows.wpname + '변경하기',
       data:response.rows
 
