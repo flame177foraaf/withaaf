6ce7e1fb149657be285bec32423218fc3cc108c2
플렝미 <forflame0@gmail.com>
2020-05-12 23:29:56 +0900

    no message

--- routes/aafwp.js
+++ routes/aafwp.js
@@ -1,4 +1,4 @@
-var express = require('express');
+aafwpvar express = require('express');
 var router = express.Router();
 var app = express();
 
@@ -11,12 +11,132 @@ const client = new Client({
 });
 
 client.connect();
-
 router.get('/', (req,res,next) => {
-  res.render('aafwp', {
+  var QueryString = "select wpid, wpname from aquafeq.aquafwp;"
+  client.query(QueryString, (err, response) => {
+    console.log(response.rows[0])
+
+    if (err) {
+      console.log('변경하기 목록 오류!')
+      res.redirect('/aafwp');
+    } else {
+      res.render('aafwp', {
+        title:'AAF 장비',
+        data:response.rows
+      });
+    };
+  });
+});
+
+router.get('/wpadd', (req,res,next) => {
+  res.render ('wpadd', {
     title:'AAF 장비'
   });
 });
+router.get('/fixwp', (req,res,next) => {
+
+  var Select_name = req.query.Seachname;
+  console.log(Select_name)
+  var QueryString = "select * from aquafeq.aquafwp where wpname = $1"
+  client.query(QueryString, [Select_name], (err, response) => {
+    res.render ('fixwp', {
+      title:'AAF 장비',
+      data:response.rows[0]
+
+    });
+  })
+
+});
+
+router.post('/fixwp', (req,res,next) => {
+  var Select_name = req.query.Seachname
+  var Wpgrade = req.body.wpgrade;
+    if (Wpgrade !== '') {
+      Wpgrade = Wpgrade.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wplimit = req.body.wplimit;
+    if (Wplimit == '') {
+        Wplimit = null
+    }
+  var Wpsocket = req.body.wpsocket;
+  var Wpether = req.body.wpether;
+  var Wpstats = req.body.wpstats;
+    if (Wpstats !== '') {
+      Wpstats = Wpstats.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wpproperty = req.body.wpproperty;
+    if (Wpproperty !== '') {
+      Wpproperty = Wpproperty.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wpfeat = req.body.wpfeat;
+    if (Wpfeat !== '') {
+      Wpfeat = Wpfeat.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wpcustom = req.body.wpcustom;
+    if (Wpcustom !== ''){
+      Wpcustom = Wpcustom.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wpup = req.body.wpup;
+    if (Wpup !== '') {
+      Wpup = Wpup.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var QueryString = "UPDATE aquafeq.aquafwp SET wpgrade = $1, wplimit = $2, wpsocket = $3, wpether = $4, wpstats = $5, wpproperty = $6, wpfeat = $7, wpcustom = $8, wpup = $9  WHERE aquafeq.aquafwp.wpname = $10 "
+  client.query(QueryString, [Wpgrade, Wplimit, Wpsocket, Wpether, Wpstats, Wpproperty, Wpfeat, Wpcustom, Wpup, Select_name], (err, response) => {
+
+    res.redirect ('/');
+  });
+
+});
+router.post('/', (req, res, next) => {
+  var Wpgrade = req.body.wpgrade;
+    if (Wpgrade !== '') {
+      Wpgrade = Wpgrade.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+
+  var Wpname = req.body.wpname;
+  var Wplimit = req.body.wplimit;
+    if (Wplimit == '') {
+        Wplimit = null
+    }
+  var Wpsocket = req.body.wpsocket;
+  var Wpether = req.body.wpether;
+  var Wpstats = req.body.wpstats;
+    if (Wpstats !== '') {
+      Wpstats = Wpstats.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wpproperty = req.body.wpproperty;
+    if (Wpproperty !== '') {
+      Wpproperty = Wpproperty.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wpfeat = req.body.wpfeat;
+    if (Wpfeat !== '') {
+      Wpfeat = Wpfeat.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wpcustom = req.body.wpcustom;
+    if (Wpcustom !== ''){
+      Wpcustom = Wpcustom.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var Wpup = req.body.wpup;
+    if (Wpup !== '') {
+      Wpup = Wpup.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    }
+  var QueryString = "INSERT INTO aquafeq.aquafwp(wpgrade, wpname, wplimit, wpsocket, wpether, wpstats, wpproperty, wpfeat, wpcustom, wpup) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);"
+  client.query(QueryString, [Wpgrade, Wpname, Wplimit, Wpsocket, Wpether, Wpstats, Wpproperty, Wpfeat, Wpcustom, Wpup], (err, response) => {
+    console.log(Wpgrade);
+    console.log(Wpname);
+    if (err) {
+      console.log('인서트 오류!')
+      res.redirect('/aafwp');
+    } else {
+      res.render('test', {
+        title:'AAF 장비'
+      });
+    };
+  });
+});
+
+
+
 
 router.get('/:id', (req,res,next) => {
   var SearchType = req.query.searchType;
