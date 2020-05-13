a597f9578376e9938c7175378b7ae9d1fe93b529
플렝미 <forflame0@gmail.com>
2020-05-13 19:09:59 +0900

    no message

--- routes/fixwp.js
+++ routes/fixwp.js
@@ -40,7 +40,7 @@ router.post('/', (req,res,next) => {
     }
   var Wplimit = req.body.wplimit;
     if (Wplimit == '') {
-        Wplimit = null
+      Wplimit = null
     }
   var Wpsocket = req.body.wpsocket;
   var Wpether = req.body.wpether;
@@ -64,7 +64,8 @@ router.post('/', (req,res,next) => {
     if (Wpup !== '') {
       Wpup = Wpup.replace(/(?:\r\n|\r|\n)/g, '<br />');
     }
-  var QueryString = "UPDATE aquafeq.aquafwp SET wpgrade = $1, wplimit = $2, wpsocket = $3, wpether = $4, wpstats = $5, wpproperty = $6, wpfeat = $7, wpcustom = $8, wpup = $9  WHERE wpname = $10"
+  var QueryString = "UPDATE aquafeq.aquafwp SET (wpgrade, wplimit, wpsocket, wpether, wpstats, wpproperty, wpfeat, wpcustom, wpup) = ($1, $2, $3, $4, $5, $6, $7, $8, $9)  WHERE wpname = $10"
+  //var QueryString = "UPDATE aquafeq.aquafwp SET wpgrade = $1, wplimit = $2, wpsocket = $3, wpether = $4, wpstats = $5, wpproperty = $6, wpfeat = $7, wpcustom = $8, wpup = $9  WHERE wpname = $10"
   client.query(QueryString, [Wpgrade, Wplimit, Wpsocket, Wpether, Wpstats, Wpproperty, Wpfeat, Wpcustom, Wpup, Select_name], (err, response) => {
     console.log(response.rows)
 
