ff88d9f1c8ffaa2cdb18da88038b86a9000c1070
플렝미 <forflame0@gmail.com>
2020-05-13 19:25:35 +0900

    no message

--- routes/fixwp.js
+++ routes/fixwp.js
@@ -64,12 +64,13 @@ router.post('/', (req,res,next) => {
     if (Wpup !== '') {
       Wpup = Wpup.replace(/(?:\r\n|\r|\n)/g, '<br />');
     }
-  var QueryString = "UPDATE aquafeq.aquafwp SET (wpgrade, wplimit, wpsocket, wpether, wpstats, wpproperty, wpfeat, wpcustom, wpup) = ($1, $2, $3, $4, $5, $6, $7, $8, $9)  WHERE wpname = $10"
-  //var QueryString = "UPDATE aquafeq.aquafwp SET wpgrade = ($1), wplimit = $2, wpsocket = $3, wpether = $4, wpstats = $5, wpproperty = $6, wpfeat = $7, wpcustom = $8, wpup = $9  WHERE wpname = $10"
+  //var QueryString = "UPDATE aquafeq.aquafwp SET (wpgrade, wplimit, wpsocket, wpether, wpstats, wpproperty, wpfeat, wpcustom, wpup) = ($1, $2, $3, $4, $5, $6, $7, $8, $9)  WHERE wpname = $10"
+  var QueryString = "UPDATE aquafeq.aquafwp SET wpgrade = $1, wplimit = $2, wpsocket = $3, wpether = $4, wpstats = $5, wpproperty = $6, wpfeat = $7, wpcustom = $8, wpup = $9  WHERE wpname = $10"
   client.query(QueryString, [Wpgrade, Wplimit, Wpsocket, Wpether, Wpstats, Wpproperty, Wpfeat, Wpcustom, Wpup, Select_name], (err, response) => {
-    console.log(Wpgrade)
-    var QueryString = "select * from aquafeq.aquafwp WHERE wpname = Select_name"
-    client.query (QueryString, (err,response) => {
+    console.log(Select_name)
+    var QueryString = "select * from aquafeq.aquafwp WHERE wpname like $1"
+    client.query (QueryString, ['%' + Select_name + '%'], (err,response) => {
+      console.log(Select_name)
       console.log(response.rows)
 
       res.render('aafwp', {
