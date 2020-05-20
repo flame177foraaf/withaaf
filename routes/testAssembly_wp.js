479f8b8c9a9851c66393c26698cc4f8185998704
플렝미 <forflame0@gmail.com>
2020-05-20 19:16:27 +0900

    no message

---
+++ routes/testAssembly_wp.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+var bodyParser = require('body-parser');
+
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  // ssl: true,
+});
+
+client.connect();
+
+router.get('/', (res,req,next) => {
+  var Assemble = req.query.assemble_to_obj;
+  var QueryString = "SELECT * FROM aquafeq.aquafwp where wpname = $1"
+  client.query(QueryString, [Assemble], (err, response) => {
+    res.render('testAssembly_wp', {
+      title: '무기 재조립하기',
+      data: response.rows
+    });
+  });
+})
+
+router.post('/', (res,req,next) => {
+
+})
+
+
+module.exports = router;
