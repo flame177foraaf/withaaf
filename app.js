fc4d9cba19d99c5c78ae8d38cb3a1ca4a5634920
플렝미 <forflame0@gmail.com>
2020-05-20 19:38:19 +0900

    no message

--- app.js
+++ app.js
@@ -11,28 +11,47 @@ var morgan = require('morgan') //로그 모듈 임포트
 //
 
 var indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+var usersRouter = require('./routes/users');
 var aafwpRouter = require('./routes/aafwp');
+app.use('/aafwp', aafwpRouter);
 //var aafwpaddRouter = require('./routes/wpAdd');
 var aafarmRouter = require('./routes/aafarm');
+app.use('/aafarm', aafarmRouter);
 //var aafarmaddRouter = require('./routes/armAdd');
 var aafaccRouter = require('./routes/aafacc');
+app.use('/aafacc', aafaccRouter);
 //var aafaccaddRouter = require('./routes/accadd');
 //var aafmixRouter = require('./routes/mix');
 var aaffeatsupRouter = require('./routes/featsup');
+app.use('/featsup', aaffeatsupRouter);
 //var aaffreedomRouter = require('./routes/index');
 //var aafQARouter = require('./routes/index');
-var aafgemRouter = require('./routes/aafgem')
-var aafrecipeRouter = require('./routes/recipe')
-var aafboardRouter = require('./routes/board')
-//var aafrecipeRouter = require('./routes/recipe2')
-var aaftestRouter = require('./routes/test')
-var aafmontableRouter =require('./routes/monster')
-var fixwpRouter = require('./routes/fixwp')
+var aafgemRouter = require('./routes/aafgem');
+app.use('/aafgem', aafgemRouter);
+var aafrecipeRouter = require('./routes/recipe');
+app.use('/recipe', aafrecipeRouter);
+var aafboardRouter = require('./routes/board');
+app.use('/board', aafboardRouter);
+//var aafrecipeRouter = require('./routes/recipe2');
+var aaftestRouter = require('./routes/test');
+app.use('/test', aaftestRouter);
+var aafmontableRouter =require('./routes/monster');
+app.use('/monster', aafmontableRouter);
+var fixwpRouter = require('./routes/fixwp');
+app.use('/fixwp', fixwpRouter);
+
+var assembleRouter = require('/routes/testAssembly');
+app.use('/testAssembly', assembleRouter);
+
+//app.use('/aafwp/wpadd', aafwpaddRouter);
+//app.use('/aafarm/armadd', aafarmaddRouter);
+//app.use('/aafacc/accadd', aafaccaddRouter);
+//app.use('/mix', aafmixRouter);
+//app.use('/qa', aafQARouter);
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var assembleRouter = require('/routes/testAssembly_wp');
 
 
 var app = express();
@@ -54,23 +73,7 @@ app.use(morgan('short')) //로그 미들웨어
 app.use(bodyParser.urlencoded({extended:true}))
 
 
-app.use('/', indexRouter);
-app.use('/aafwp', aafwpRouter);
-//app.use('/aafwp/wpadd', aafwpaddRouter);
-app.use('/aafarm', aafarmRouter);
-//app.use('/aafarm/armadd', aafarmaddRouter);
-app.use('/aafacc', aafaccRouter);
-//app.use('/aafacc/accadd', aafaccaddRouter);
-//app.use('/mix', aafmixRouter);
-app.use('/featsup', aaffeatsupRouter)
-app.use('/aafgem', aafgemRouter)
-app.use('/recipe', aafrecipeRouter)
-app.use('/board', aafboardRouter);
-//app.use('/qa', aafQARouter);
-app.use('/test', aaftestRouter);
-app.use('/monster', aafmontableRouter);
-app.use('/fixwp', fixwpRouter);
-app.use('/testAssembly_wp', assembleRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
