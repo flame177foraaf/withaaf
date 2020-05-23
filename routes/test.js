c57ae6685090370d9e7dbccfe5df85b6a6b84c41
플렝미 <forflame0@gmail.com>
2020-05-23 16:15:55 +0900

    no message

--- routes/test.js
+++ routes/test.js
@@ -11,322 +11,218 @@ const client = new Client({
 
 client.connect();
 
+
 router.get('/', (req,res,next) => {
-  var QueryString = "select wpid, wpname from aquafeq.aquafwp;"
-  client.query(QueryString, (err, response) => {
-    console.log(response.rows[0])
+  var Assembly = req.query.assembly;
 
-    if (err) {
-      console.log('변경하기 목록 오류!')
-      res.redirect('/test');
-    } else {
-      res.render('test', {
-        title:'AAF 장비',
-        data:response.rows
-      });
-    };
-  });
-});
-/*
-router.get('/wpadd', (req,res,next) => {
-  res.render ('wpadd', {
-    title:'AAF 장비'
-  });
-});
-router.get('/fixwp', (req,res,next) => {
-
-  var Select_name = req.query.Seachname;
-  console.log(Select_name)
-  var QueryString = "select * from aquafeq.aquafwp where wpname = $1"
-  client.query(QueryString, [Select_name], (err, response) => {
-    res.render ('fixwp', {
-      title:'AAF 장비',
-      data:response.rows[0]
-
-    });
-  })
-
-});
-router.get('/testAssembly', (res,req,next) => {
-  var Assemble = req.query.assemble;
-  console.log(req.query.assemble);
-  console.log(Assemble);
-
-
-  var QueryString = "SELECT * FROM aquafeq.aquafwp where wpname = $1"
-  client.query(QueryString, [Assemble], (err, response) => {
-    res.render('testAssembly', {
+  var QueryString = "SELECT * FROM aquafeq.aquafwp where wpname = $1";
+  client.query(QueryString, [Assembly],(err, response) => {
+    res.render('test', {
       title: '무기 재조립하기',
       data: response.rows[0]
     });
   });
 });
-*/
-router.post('/fixwp', (req,res,next) => {
-  var Select_name = req.query.Seachname
-  var Wpgrade = req.body.wpgrade;
-    if (Wpgrade !== '') {
-      Wpgrade = Wpgrade.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
-  var Wplimit = req.body.wplimit;
-    if (Wplimit == '') {
-        Wplimit = null
-    }
-  var Wpsocket = req.body.wpsocket;
-  var Wpether = req.body.wpether;
-  var Wpstats = req.body.wpstats;
-    if (Wpstats !== '') {
-      Wpstats = Wpstats.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
-  var Wpproperty = req.body.wpproperty;
-    if (Wpproperty !== '') {
-      Wpproperty = Wpproperty.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
-  var Wpfeat = req.body.wpfeat;
-    if (Wpfeat !== '') {
-      Wpfeat = Wpfeat.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
-  var Wpcustom = req.body.wpcustom;
-    if (Wpcustom !== ''){
-      Wpcustom = Wpcustom.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
-  var Wpup = req.body.wpup;
-    if (Wpup !== '') {
-      Wpup = Wpup.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
-  var QueryString = "UPDATE aquafeq.aquafwp SET wpgrade = $1, wplimit = $2, wpsocket = $3, wpether = $4, wpstats = $5, wpproperty = $6, wpfeat = $7, wpcustom = $8, wpup = $9  WHERE aquafeq.aquafwp.wpname = $10 "
-  client.query(QueryString, [Wpgrade, Wplimit, Wpsocket, Wpether, Wpstats, Wpproperty, Wpfeat, Wpcustom, Wpup, Select_name], (err, response) => {
 
-    res.redirect ('/');
+router.get('/ing', (req,res,next) => {
+  var Assembly = req.query.assembly
+  var QueryString = "SELECT * FROM aquafeq.aquafwp where wpname = $1";
+  client.query(QueryString, [Assembly], (err, response) => {
+    console.log(response.rows[0].wpcustom);
+
+    var Allcustom = response.rows[0].wpcustom;
+    console.log(Allcustom);
+    var eqcustom = Allcustom.split('<br />');
+    var result_custom = 'null';
+
+    function Dice_roll(min, max){   //주사위 굴리기
+      var diceroll = max - min + 1;
+      return Math.floor(Math.random() * diceroll + min);
+    }
+
+    // 커스텀 재조립
+    for (var i = 0; i < eqcustom.length; i++) {
+      if (eqcustom[i].indexOf("웨이블렘") == -1) {
+        var find_cus_val1 = eqcustom[i].indexOf("(");
+        var find_cus_val2 = eqcustom[i].indexOf(")");
+        var find_cus_per1 = eqcustom[i].indexOf("[");
+        var find_cus_per2 = eqcustom[i].indexOf("]");
+        var cut_cus_name = eqcustom[i].substring(0,find_cus_val1);
+        var cut_cus_value = eqcustom[i].substring(find_cus_val1+1,find_cus_val2); //커스텀 수치
+        //var random_num = Math.floor(Math.random() * 10 + 1);
+        var leng = eqcustom[i].length;
+        var cus_per = eqcustom[i].substring(find_cus_per1+1,find_cus_per2); // 대괄호
+        var cus_per_0 = cus_per.indexOf("%");
+        var cus_per_1 = cus_per.substring(0,cus_per_0);  // 커스텀 뜰 확률
+        var cus_per_2 = Math.floor(Math.random() * 100) + 1
+        if (cus_per_2 <= cus_per_1) {
+          var cut_in_custom = cut_cus_value.indexOf("~");
+          var find_per_custom = cut_cus_value.indexOf("%");
+          var find_minus_custom = cut_cus_value.indexOf("-");
+          var custommin = cut_cus_value.substring(0,cut_in_custom); //커스텀 수치 최소  3 %
+          var custommax = cut_cus_value.substring(cut_in_custom+1); //커스텀 수치 최대    20 %
+          if (find_per_custom !== -1) {
+            var find_per_cusmin = custommin.indexOf("%");
+            var find_per_cusmax = custommax.indexOf("%");
+            var custommin = custommin.substring(0,find_per_cusmin);
+            var custommax = custommax.substring(0, find_per_cusmax);
+
+            var rancustom = Dice_roll(parseInt(custommin), parseInt(custommax));
+            if (rancustom !== 0) {
+              var rancustom = rancustom + ' %';
+            }
+          } else {
+
+            var rancustom = Dice_roll(parseInt(custommin), parseInt(custommax));
+          }
+          if (rancustom !== 0) {
+            if (find_minus_custom !== -1) {
+              if (result_custom === 'null') {
+                var result_custom = cut_cus_name + rancustom;
+              } else {
+                var result_custom = result_custom +"<br />" + cut_cus_name + rancustom;
+              }
+            } else {
+              if (result_custom === 'null') {
+                var result_custom = cut_cus_name +"+ "+ rancustom;
+              } else {
+                var result_custom = result_custom +"<br />" + cut_cus_name +"+ "+ rancustom;
+              }
+            }
+          }
+        }
+      } else {
+        var find_cus_val1 = eqcustom[i].indexOf("("); //웨이블렘 괄호
+        var find_cus_val2 = eqcustom[i].indexOf(")"); //웨이블렘 괄호
+        var find_cus_val1 = eqcustom[i].indexOf("(",find_cus_val1 +1);
+        var find_cus_val2 = eqcustom[i].indexOf(")",find_cus_val2 +1);
+        var find_cus_per1 = eqcustom[i].indexOf("[");
+        var find_cus_per2 = eqcustom[i].indexOf("]");
+        var cut_cus_name = eqcustom[i].substring(0,find_cus_val1);
+        var cut_cus_value = eqcustom[i].substring(find_cus_val1+1,find_cus_val2); //커스텀 수치
+
+
+        //var random_num = Math.floor(Math.random() * 10 + 1);
+        var leng = eqcustom[i].length;
+
+
+        var cus_per = eqcustom[i].substring(find_cus_per1+1,find_cus_per2); // 대괄호
+        var cus_per_0 = cus_per.indexOf("%");
+        var cus_per_1 = cus_per.substring(0,cus_per_0);  // 커스텀 뜰 확률
+        var cus_per_2 = Math.floor(Math.random() * 100) + 1
+
+        if (cus_per_2 <= cus_per_1) {
+          var cut_in_custom = cut_cus_value.indexOf("~");
+          var find_per_custom = cut_cus_value.indexOf("%");
+          var find_minus_custom = cut_cus_value.indexOf("-");
+          var custommin = cut_cus_value.substring(0,cut_in_custom); //커스텀 수치 최소  3 %
+          var custommax = cut_cus_value.substring(cut_in_custom+1); //커스텀 수치 최대    20 %
+          if (find_per_custom !== -1) {
+            var find_per_cusmin = custommin.indexOf("%");
+            var find_per_cusmax = custommax.indexOf("%");
+            var custommin = custommin.substring(0,find_per_cusmin);
+            var custommax = custommax.substring(0, find_per_cusmax);
+
+            var rancustom = Dice_roll(parseInt(custommin), parseInt(custommax));
+
+            if (rancustom !== 0) {
+              var rancustom = rancustom + ' %';
+            }
+          } else {
+
+            var rancustom = Dice_roll(parseInt(custommin), parseInt(custommax));
+          }
+          if (rancustom !== 0) {
+            if (find_minus_custom !== -1) {
+              if (result_custom === 'null') {
+                var result_custom = cut_cus_name + rancustom;
+              } else {
+                var result_custom = result_custom +"<br />" + cut_cus_name + rancustom;
+              }
+            } else {
+              if (result_custom === 'null') {
+                var result_custom = cut_cus_name +"+ "+ rancustom;
+              } else {
+                var result_custom = result_custom +"<br />" + cut_cus_name +"+ "+ rancustom;
+              }
+            }
+          }
+
+        }
+      }
+    }
+
+    //소켓 재조
+    var Allsocket = response.rows[0].wpsocket
+    //console.log("소켓" + testsocket.length)
+    //console.log("소켓 구분" + testsocket.indexOf("~"))
+
+    var minsocket = Allsocket.substring(0,Allsocket.indexOf("~"));
+    var maxsocket = Allsocket.substring(Allsocket.indexOf("~")+1,Allsocket.length)
+    var socket = "";
+    var socketx = "";
+    var socket_roll = Dice_roll(parseInt(minsocket),parseInt(maxsocket)) //소켓 범위 내에서 굴리기
+
+    for (var i = 0; i < socket_roll; i++) {
+      socket = socket + "○"
+    }
+    for (var j = parseInt(maxsocket)-socket_roll ; j > 0; j--) {
+      socketx = socketx + "●"
+    }
+    var result_socket = socket + socketx;
+    //console.log("소켓 주사위 굴리기" +socket_roll)
+    //console.log(socket + socketx)
+
+    // 스텟 재조립
+    var ALlstats = response.rows[0].wpstats
+    // console.log(ALlstats.indexOf("/"))
+    var cut1_stats = ALlstats.substring(0,ALlstats.indexOf("/"))
+    var cut2_stats = ALlstats.substring(ALlstats.indexOf("/")+1)
+    //console.log(cut1_stats)
+    //console.log(cut2_stats)
+    var first_stat = cut1_stats.substring(cut1_stats.indexOf("+") +1)
+    var second_stat = cut2_stats.substring(cut2_stats.indexOf("+") +1)
+    //console.log(first_stat)
+    //console.log(second_stat)
+    //console.log(parseInt(first_stat))
+
+    var Dice_roll_first_stat = parseInt(Dice_roll(-10,10))
+    //console.log(Dice_roll_first_stat)
+    var Dice_roll_first_stat_per =  (100 + Dice_roll_first_stat  )/100
+    //console.log(Dice_roll_first_stat_per)
+    var first_stat = parseInt(first_stat)*Dice_roll_first_stat_per
+    var first_stat = Math.floor(first_stat)  //소수점 버리기
+    if (Dice_roll_first_stat > 0) {
+      var result_first_stats = first_stat + "(" +" + "+ Dice_roll_first_stat + " % " + ")";
+    }else {
+      var result_first_stats =first_stat + " ( " + Dice_roll_first_stat + " % " + ") ";
+
+    }
+
+    var Dice_roll_second_stat = parseInt(Dice_roll(-10,10))
+    //console.log(Dice_roll_second_stat)
+    var Dice_roll_second_stat_per =  (100 + Dice_roll_second_stat  )/100
+    //console.log(Dice_roll_second_stat_per)
+    var second_stat = parseInt(second_stat)*Dice_roll_second_stat_per
+    var second_stat = Math.floor(second_stat) //소수점 버리기
+    if (Dice_roll_second_stat > 0) {
+      var result_second_stats = second_stat + "(" +" + "+ Dice_roll_second_stat + " % " + ")";
+    }else {
+      var result_second_stats =second_stat + " ( " + Dice_roll_second_stat + " % " + ") ";
+    }
+    var result_stats = result_first_stats +" / " +result_second_stats;
+
+
+    res.render('test', {
+      title: '무기 재조립하기',
+      data: response.rows[0],
+      wpsocket: result_socket,
+      wpcustom: result_custom,
+      wpstats:result_stats,
+    });
   });
+})
 
-});
-router.post('/', (req, res, next) => {
-  var Wpgrade = req.body.wpgrade;
-    if (Wpgrade !== '') {
-      Wpgrade = Wpgrade.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
 
-  var Wpname = req.body.wpname;
-  var Wplimit = req.body.wplimit;
-    if (Wplimit == '') {
-        Wplimit = null
-    }
-  var Wpsocket = req.body.wpsocket;
-  var Wpether = req.body.wpether;
-  var Wpstats = req.body.wpstats;
-    if (Wpstats !== '') {
-      Wpstats = Wpstats.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
-  var Wpproperty = req.body.wpproperty;
-    if (Wpproperty !== '') {
-      Wpproperty = Wpproperty.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
-  var Wpfeat = req.body.wpfeat;
-    if (Wpfeat !== '') {
-      Wpfeat = Wpfeat.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
-  var Wpcustom = req.body.wpcustom;
-    if (Wpcustom !== ''){
-      Wpcustom = Wpcustom.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
-  var Wpup = req.body.wpup;
-    if (Wpup !== '') {
-      Wpup = Wpup.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    }
-  var QueryString = "INSERT INTO aquafeq.aquafwp(wpgrade, wpname, wplimit, wpsocket, wpether, wpstats, wpproperty, wpfeat, wpcustom, wpup) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);"
-  client.query(QueryString, [Wpgrade, Wpname, Wplimit, Wpsocket, Wpether, Wpstats, Wpproperty, Wpfeat, Wpcustom, Wpup], (err, response) => {
-    console.log(Wpgrade);
-    console.log(Wpname);
-    if (err) {
-      console.log('인서트 오류!')
-      res.redirect('/test');
-    } else {
-      res.render('test', {
-        title:'AAF 장비'
-      });
-    };
-  });
-});
-router.get('/:id', (req,res,next) => {
-  var SearchType = req.query.searchType;
-  if (SearchType === 'name') {
-    var Search = req.query.searchText;
-    var CurrentPage = req.params.id;
-    var SearchLimit = req.query.limit;
-    if (SearchLimit === undefined) {
-      SearchLimit = 0;
-    };
-    console.log(SearchLimit)
-    var QueryString = "SELECT *, count(*) over() as totalcount FROM aquafeq.aquafwp where wpname LIKE $1 and cast(wplimit as INTEGER) >= $2 or wplimit is null  ORDER BY wplimit,wpid asc limit 10 offset (($3- 1)*10);"
-    client.query(QueryString, ['%' + Search + '%', SearchLimit, CurrentPage], (err, response) => {
-      console.log('서치리밋' + SearchLimit);
-      if(typeof(response.rows[0]) !== "object") {
-        var TotalCount = 1;
-      } else {
-        var TotalCount = response.rows[0].totalcount;
-      }
-      console.log('토탈 카운트 ' + TotalCount)
-      console.log(CurrentPage)
-      console.log(typeof(CurrentPage))
-      var DataCountInPage = 10;
-      var PageSize = 10;
-      var TotalPage = parseInt(TotalCount / DataCountInPage,10);
-      if (TotalCount % DataCountInPage > 0) {
-        TotalPage++;
-      };
-      console.log('토탈 페이지' + TotalPage);
-      if (TotalPage < CurrentPage) {
-        CurrentPage = TotalPage;
-      };
-      var StartPage = parseInt(((CurrentPage - 1)/10),10) *10 +1;
-      console.log('스타트페이지' + StartPage);
-      var EndPage = StartPage + DataCountInPage -1;
-      if (EndPage > TotalPage) {
-        EndPage = TotalPage;
-      };
-      console.log('엔드페이지'+ EndPage);
-      res.render('test', {
-        title: 'AAF 장비',
-        data: response.rows,
-        CurrentPage: CurrentPage,
-        PageSize: PageSize,
-        StartPage: StartPage,
-        EndPage: EndPage,
-        TotalPage: TotalPage,
-        SearchType: SearchType,
-        Search: Search,
-        SearchLimit: SearchLimit
-      });
-    });
-  } else if (SearchType === 'property') {
-    var Search = req.query.searchText;
-    var CurrentPage = req.params.id
-    var SearchLimit = req.query.limit;
-    if (SearchLimit === undefined) {
-      SearchLimit = 0;
-    };
-    var QueryString = "SELECT *, count(*) over() as totalcount FROM aquafeq.aquafwp where wpproperty LIKE $1 and cast(wplimit as INTEGER) >= $2 or wplimit is null  ORDER BY wplimit asc limit 10 offset (($3- 1)*10);"
-    client.query(QueryString, ['%' + Search + '%', SearchLimit, CurrentPage], (err, response) => {
-      if(typeof(response.rows[0]) !== "object") {
-        var TotalCount = 1;
-      } else {
-        var TotalCount = response.rows[0].totalcount;
-      }
-      console.log(TotalCount)
-      var DataCountInPage = 10;
-      var PageSize = 10;
-      var TotalPage = parseInt(TotalCount / DataCountInPage,10);
-      if (TotalCount % DataCountInPage > 0) {
-        TotalPage++;
-      };
-      if (TotalPage < CurrentPage) {
-        CurrentPage = TotalPage;
-      };
-      var StartPage = parseInt(((CurrentPage - 1)/10),10) *10 +1;
-      var EndPage = StartPage + DataCountInPage -1;
-      if (EndPage > TotalPage) {
-        EndPage = TotalPage;
-      };
-      res.render('test', {
-        title: 'AAF 장비',
-        data: response.rows,
-        CurrentPage: CurrentPage,
-        PageSize: PageSize,
-        StartPage: StartPage,
-        EndPage: EndPage,
-        TotalPage:TotalPage,
-        SearchType: SearchType,
-        Search: Search,
-        SearchLimit: SearchLimit
-      });
-    });
-  } else if (SearchType === 'feat') {
-    var Search = req.query.searchText;
-    var CurrentPage = req.params.id
-    var SearchLimit = req.query.limit;
-    if (SearchLimit === undefined) {
-      SearchLimit = 0;
-    };
-    var QueryString = "SELECT *, count(*) over() as totalcount FROM aquafeq.aquafwp where wpfeat LIKE $1 and cast(wplimit as INTEGER) >= $2 or wplimit is null  ORDER BY wplimit asc limit 10 offset (($3- 1)*10);"
-    client.query(QueryString, ['%' + Search + '%', SearchLimit, CurrentPage], (err, response) => {
-      if(typeof(response.rows[0]) !== "object") {
-        var TotalCount = 1;
-      } else {
-        var TotalCount = response.rows[0].totalcount;
-      }
-      console.log(TotalCount)
-      var DataCountInPage = 10;
-      var PageSize = 10;
-      var TotalPage = parseInt(TotalCount / DataCountInPage,10);
-      if (TotalCount % DataCountInPage > 0) {
-        TotalPage++;
-      };
-      if (TotalPage < CurrentPage) {
-        CurrentPage = TotalPage;
-      };
-      var StartPage = parseInt(((CurrentPage - 1)/10),10) *10 +1;
-      var EndPage = StartPage + DataCountInPage -1;
-      if (EndPage > TotalPage) {
-        EndPage = TotalPage;
-      };
-      res.render('test', {
-        title: 'AAF 장비',
-        data: response.rows,
-        CurrentPage: CurrentPage,
-        PageSize: PageSize,
-        StartPage: StartPage,
-        EndPage: EndPage,
-        TotalPage:TotalPage,
-        SearchType: SearchType,
-        Search: Search,
-        SearchLimit: SearchLimit
-      });
-    });
-  } else if (SearchType === 'custom') {
-    var Search = req.query.searchText;
-    var CurrentPage = req.params.id
-    var SearchLimit = req.query.limit;
-    if (SearchLimit === undefined) {
-      SearchLimit = 0;
-    };
-    var QueryString = "SELECT *, count(*) over() as totalcount FROM aquafeq.aquafwp where wpcustom LIKE $1 and cast(wplimit as INTEGER) >= $2 or wplimit is null  ORDER BY wplimit asc limit 10 offset (($3- 1)*10);"
-    client.query(QueryString, ['%' + Search + '%', SearchLimit, CurrentPage], (err, response) => {
-      if(typeof(response.rows[0]) !== "object") {
-        var TotalCount = 1;
-      } else {
-        var TotalCount = response.rows[0].totalcount;
-      }
-      console.log(TotalCount)
-      var DataCountInPage = 10;
-      var PageSize = 10;
-      var TotalPage = parseInt(TotalCount / DataCountInPage,10);
-      if (TotalCount % DataCountInPage > 0) {
-        TotalPage++;
-      };
-      if (TotalPage < CurrentPage) {
-        CurrentPage = TotalPage;
-      };
-      var StartPage = parseInt(((CurrentPage - 1)/10),10) *10 +1;
-      var EndPage = StartPage + DataCountInPage -1;
-      if (EndPage > TotalPage) {
-        EndPage = TotalPage;
-      };
-      res.render('test', {
-        title: 'AAF 장비',
-        data: response.rows,
-        CurrentPage: CurrentPage,
-        PageSize: PageSize,
-        StartPage: StartPage,
-        EndPage: EndPage,
-        TotalPage:TotalPage,
-        SearchType: SearchType,
-        Search: Search,
-        SearchLimit: SearchLimit
-      });
-    });
-  } else {
-    res.redirect('/test');
-  };
-});
+
 module.exports = router;
