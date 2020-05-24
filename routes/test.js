f926cf3d8ed118cdbf727e1c6579d62650122501
플렝미 <forflame0@gmail.com>
2020-05-24 13:54:42 +0900

    no message

--- routes/test.js
+++ routes/test.js
@@ -24,7 +24,7 @@ router.get('/', (req,res,next) => {
     });
   });
 });
-/*
+
 router.get('/ing', (req,res,next) => {
   var Assembly = '쿼드 혼';
   var QueryString = "SELECT * FROM aquafeq.aquafwp where wpname = $1";
@@ -233,7 +233,7 @@ router.get('/ing', (req,res,next) => {
     res.send({data:Alldata})
   });
 })
-*/
+
 
 
 module.exports = router;
