4d1f06ba8634ebc9842ce54b29cdcc76d6e3548f
플렝미 <forflame0@gmail.com>
2020-05-26 18:12:29 +0900

    no message

--- routes/test.js
+++ routes/test.js
@@ -24,8 +24,8 @@ router.get('/:id', (req,res,next) => {
     var Search = req.query.searchText;
     var CurrentPage = req.params.id;
     var Collate = "ko_KR.utf8";
-    var QueryString = "SELECT *, count(*) over() as totalcount FROM aquafeq.aquafitem where item_name LIKE $1 limit 10 offset (($2- 1)*10);"
-    client.query(QueryString, ['%' + Search + '%', CurrentPage ], (err, response) => {
+    var QueryString = "SELECT *, count(*) over() as totalcount FROM aquafeq.aquafitem where item_name LIKE $1 ORDER BY item_name collate $3 limit 10 offset (($2- 1)*10);"
+    client.query(QueryString, ['%' + Search + '%', CurrentPage, Collate], (err, response) => {
       console.log(QueryString)
       console.log(response.rows)
       if(typeof(response.rows[0]) !== "object") {
