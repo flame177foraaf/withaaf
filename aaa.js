e452f7ad2742bb367c8c27aeb2a5c64c61710766
플렝미 <forflame0@gmail.com>
2020-05-28 18:42:00 +0900

    no message

--- aaa.js
+++ aaa.js
@@ -1,101 +1,151 @@
+var Allcustom = "크리티컬 데미지 변화 ( 8 % ~ 15 % ) [100% 확률]'<br />'상세 속성 추가 데미지 변화 ( 8 % ~ 14 % ) [100% 확률]'<br />'실드 HP 추가 ( 2 % ~ 5 % ) [60% 확률]'<br />'크리티컬률 변화 ( 3 % ~ 6 % ) [100% 확률]'<br />'크리티컬률 변화 ( 3 % ~ 6 % ) [20% 확률]'<br />'크리티컬 데미지 변화 ( 5 % ~ 13 % ) [30% 확률]'<br />'소울 루비 추출 기능 ( 5 ~ 15 ) [20% 확률]'<br />'소울 사파이어 추출 기능 ( 5 ~ 15 ) [20% 확률]";
+//console.log(Allcustom);
+var eqcustom = Allcustom.split('<br />');
+var result_custom = 'null';
 
+  var Special = "checked";
+  var Reinforce = "checked";
 
-var Allproperty = "(3600/3500/0/0/5000/-3000) <br> * 강화비용 6 배 증가 * <br>* 강화성공률 10 % 감소 *";
-
-var Special = 'checked';
-var Reinforce = 'checked';
-function Dice_roll(min, max){   //주사위 굴리기
-  var diceroll = max - min + 1;
-  return Math.floor(Math.random() * diceroll + min);
-}
-var result_property = 'null'
-
-var cut1_property = Allproperty.indexOf("(")
-var cut2_property = Allproperty.indexOf(")")
-var cut_property = Allproperty.substring(cut1_property+1,cut2_property)
-var cuts_property = cut_property.split("/")
-
-
-for (var i = 0; i < cuts_property.length; i++) {
-  if (Special === "checked") {
-    var random = Dice_roll(-20,40)
-  } else {
-    var random = Dice_roll(-30,30)
+  function Dice_roll(min, max){   //주사위 굴리기
+    var diceroll = max - min + 1;
+    return Math.floor(Math.random() * diceroll + min);
   }
+  console.log(Allcustom)
+  console.log(eqcustom)
 
-  if (parseInt(cuts_property[i]) == 0) {
-    var eachproperty = 0;
-    if (result_property === 'null') {
-      var result_property = eachproperty
-    } else {
-      var result_property = result_property + " / " + eachproperty
-    }
-  } else if (parseInt(cuts_property[i]) > 0 ) {
-    //속방 수치가 0 이상일때
-    if (Reinforce == "checked") {
-      var Reinforce1 = 1.8435;
-    } else {
-      var Reinforce1 = 1;
-    }
-    if (random == 0) {
-      var eachproperty = parseInt(cuts_property[i]);
-      if (result_property === 'null') {
-        var result_property = eachproperty + " ( " + random +" % "+ ")"
-      } else {
-        var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
-      }
-    } else if ( random > 0 ){
-      var ranproperty = (100 + random )/100
-      var eachproperty = Math.round(parseInt(cuts_property[i])*Reinforce1*ranproperty)
-      console.log(eachproperty)
-      if (result_property === 'null') {
-        var result_property = eachproperty + " ( " + random +" % "+ ")"
-      } else {
-        var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
-      }
-    } else {
+if (Allcustom === "") {
+  var result_custom = "재조립할 커스텀이 없네요~_~";
 
-      var ranproperty = (100 + random )/100
-      var eachproperty = Math.round(parseInt(cuts_property[i])*Reinforce1*ranproperty)
-      if (result_property === 'null') {
-        var result_property = eachproperty + " ( " + random +" % "+ ")"
-      } else {
-        var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
-      }
-    }
+} else {
+  for (var i = 0; i < eqcustom.length; i++) {
 
-  } else if (parseInt(cuts_property[i]) < 0 ){
-    if (Reinforce == "checked") {
-      var Reinforce1 = Math.pow(1.07,5);
+    if (eqcustom[i].indexOf('[') === -1) {
+      var result_custom = "커스텀의 정보가 정확하지 않습니다." +"<br />"+"커스텀이 적용될 확률이 분명하게 표시되어있는지 확인해주세요."
     } else {
-      var Reinforce1 = 1;
-    }
+      if (eqcustom[i].indexOf("웨이블렘") == -1) {
+        var find_cus_val1 = eqcustom[i].indexOf("(");
+        var find_cus_val2 = eqcustom[i].indexOf(")");
+        var find_cus_per1 = eqcustom[i].indexOf("[");
+        var find_cus_per2 = eqcustom[i].indexOf("]");
+        var cut_cus_name = eqcustom[i].substring(0,find_cus_val1);
+        var cut_cus_value = eqcustom[i].substring(find_cus_val1+1,find_cus_val2); //커스텀 수치
+        //var random_num = Math.floor(Math.random() * 10 + 1);
+        var leng = eqcustom[i].length;
+        var cus_per = eqcustom[i].substring(find_cus_per1+1,find_cus_per2); // 대괄호
+        var cus_per_0 = cus_per.indexOf("%");
+        var cus_per_1 = cus_per.substring(0,cus_per_0);  // 커스텀 뜰 확률
+        if (Special == "checked") {
+          cus_per_1 = parseInt(cus_per.substring(0,cus_per_0)) + 30
+          if (cus_per_1 > 100) {
+            cus_per_1 = 100;
+          }
+        }
 
-    if (random == 0) {
-      var eachproperty = parseInt(cuts_property[i]);
-      if (result_property === 'null') {
-        var result_property = eachproperty + " ( " + random +" % "+ ")"
+        var cus_per_2 = Math.floor(Math.random() * 100) + 1
+
+        if (cus_per_2 <= cus_per_1) {
+          var cut_in_custom = cut_cus_value.indexOf("~");
+          var find_per_custom = cut_cus_value.indexOf("%");
+          var find_minus_custom = cut_cus_value.indexOf("-");
+          var custommin = cut_cus_value.substring(0,cut_in_custom); //커스텀 수치 최소  3 %
+          var custommax = cut_cus_value.substring(cut_in_custom+1); //커스텀 수치 최대    20 %
+          if (find_per_custom !== -1) {
+            var find_per_cusmin = custommin.indexOf("%");
+            var find_per_cusmax = custommax.indexOf("%");
+            var custommin = custommin.substring(0,find_per_cusmin);
+            var custommax = custommax.substring(0, find_per_cusmax);
+
+            var rancustom = Dice_roll(parseInt(custommin), parseInt(custommax));
+            if (rancustom !== 0) {
+              var rancustom = rancustom + ' %';
+            }
+          } else {
+
+            var rancustom = Dice_roll(parseInt(custommin), parseInt(custommax));
+          }
+          if (rancustom !== 0) {
+            if (find_minus_custom !== -1) {
+              if (result_custom === 'null') {
+                var result_custom = cut_cus_name + rancustom;
+              } else {
+                var result_custom = result_custom +"<br />" + cut_cus_name + rancustom;
+              }
+            } else {
+              if (result_custom === 'null') {
+                var result_custom = cut_cus_name +"+ "+ rancustom;
+              } else {
+                var result_custom = result_custom +"<br />" + cut_cus_name +"+ "+ rancustom;
+              }
+            }
+          }
+        }
       } else {
-        var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
-      }
-    } else if ( random > 0 ){
-      var ranproperty = (100 + random )/100
-      var eachproperty = Math.round(parseInt(cuts_property[i])*Reinforce1*ranproperty)
-      if (result_property === 'null') {
-        var result_property = eachproperty + " ( " + random +" % "+ ")"
-      } else {
-        var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
-      }
-    } else {
-      var ranproperty = (100 + random )/100
-      var eachproperty = Math.round(parseInt(cuts_property[i])*Reinforce1*ranproperty)
-      if (result_property === 'null') {
-        var result_property = eachproperty + " ( " + random +" % "+ ")"
-      } else {
-        var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
+        var find_cus_val1 = eqcustom[i].indexOf("("); //웨이블렘 괄호
+        var find_cus_val2 = eqcustom[i].indexOf(")"); //웨이블렘 괄호
+        var find_cus_val1 = eqcustom[i].indexOf("(",find_cus_val1 +1);
+        var find_cus_val2 = eqcustom[i].indexOf(")",find_cus_val2 +1);
+        var find_cus_per1 = eqcustom[i].indexOf("[");
+        var find_cus_per2 = eqcustom[i].indexOf("]");
+        var cut_cus_name = eqcustom[i].substring(0,find_cus_val1);
+        var cut_cus_value = eqcustom[i].substring(find_cus_val1+1,find_cus_val2); //커스텀 수치
+
+
+        //var random_num = Math.floor(Math.random() * 10 + 1);
+        var leng = eqcustom[i].length;
+
+
+        var cus_per = eqcustom[i].substring(find_cus_per1+1,find_cus_per2); // 대괄호
+        var cus_per_0 = cus_per.indexOf("%");
+        var cus_per_1 = cus_per.substring(0,cus_per_0);  // 커스텀 뜰 확률
+        if (Special == "checked") {
+          cus_per_1 = parseInt(cus_per.substring(0,cus_per_0)) + 30
+          if (cus_per_1 > 100) {
+            cus_per_1 = 100;
+          }
+        }
+
+        var cus_per_2 = Math.floor(Math.random() * 100) + 1
+
+        if (cus_per_2 <= cus_per_1) {
+          var cut_in_custom = cut_cus_value.indexOf("~");
+          var find_per_custom = cut_cus_value.indexOf("%");
+          var find_minus_custom = cut_cus_value.indexOf("-");
+          var custommin = cut_cus_value.substring(0,cut_in_custom); //커스텀 수치 최소  3 %
+          var custommax = cut_cus_value.substring(cut_in_custom+1); //커스텀 수치 최대    20 %
+          if (find_per_custom !== -1) {
+            var find_per_cusmin = custommin.indexOf("%");
+            var find_per_cusmax = custommax.indexOf("%");
+            var custommin = custommin.substring(0,find_per_cusmin);
+            var custommax = custommax.substring(0, find_per_cusmax);
+
+            var rancustom = Dice_roll(parseInt(custommin), parseInt(custommax));
+
+            if (rancustom !== 0) {
+              var rancustom = rancustom + ' %';
+            }
+          } else {
+
+            var rancustom = Dice_roll(parseInt(custommin), parseInt(custommax));
+          }
+          if (rancustom !== 0) {
+            if (find_minus_custom !== -1) {
+              if (result_custom === 'null') {
+                var result_custom = cut_cus_name + rancustom;
+              } else {
+                var result_custom = result_custom +"<br />" + cut_cus_name + rancustom;
+              }
+            } else {
+              if (result_custom === 'null') {
+                var result_custom = cut_cus_name +"+ "+ rancustom;
+              } else {
+                var result_custom = result_custom +"<br />" + cut_cus_name +"+ "+ rancustom;
+              }
+            }
+          }
+
+        }
       }
     }
   }
 }
-
-console.log(result_property)
+console.log(result_custom)
--- routes/Assemblyarm.js
+++ routes/Assemblyarm.js
@@ -69,6 +69,7 @@ router.get('/ing', (req,res,next) => {
         result_property:"null"
       };
     } else {
+      console.log(response.rows[0].armproperty)
       function Dice_roll(min, max){   //주사위 굴리기
         var diceroll = max - min + 1;
         return Math.floor(Math.random() * diceroll + min);
@@ -77,97 +78,7 @@ router.get('/ing', (req,res,next) => {
       //console.log(response.rows[0].armcustom);
       //console.log(response.rows[0])
 
-      //속방 재조립
-      var result_property = 'null'
-      var Allproperty = response.rows[0].armproperty;
 
-      var cut1_property = Allproperty.indexOf("(")
-      var cut2_property = Allproperty.indexOf(")")
-      var cut_property = Allproperty.substring(cut1_property+1,cut2_property)
-      var cuts_property = cut_property.split("/")
-
-
-      for (var i = 0; i < cuts_property.length; i++) {
-        if (Special === "checked") {
-          var random = Dice_roll(-20,40)
-        } else {
-          var random = Dice_roll(-30,30)
-        }
-
-        if (parseInt(cuts_property[i]) == 0) {
-          var eachproperty = 0;
-          if (result_property === 'null') {
-            var result_property = eachproperty
-          } else {
-            var result_property = result_property + " / " + eachproperty
-          }
-        } else if (parseInt(cuts_property[i]) > 0 ) {
-          //속방 수치가 0 이상일때
-          if (Reinforce == "checked") {
-            var Reinforce1 = 1.8435;
-          } else {
-            var Reinforce1 = 1;
-          }
-          if (random == 0) {
-            var eachproperty = parseInt(cuts_property[i]);
-            if (result_property === 'null') {
-              var result_property = eachproperty + " ( " + random +" % "+ ")"
-            } else {
-              var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
-            }
-          } else if ( random > 0 ){
-            var ranproperty = (100 + random )/100
-            var eachproperty = Math.round(parseInt(cuts_property[i])*Reinforce1*ranproperty)
-            console.log(eachproperty)
-            if (result_property === 'null') {
-              var result_property = eachproperty + " ( " + random +" % "+ ")"
-            } else {
-              var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
-            }
-          } else {
-
-            var ranproperty = (100 + random )/100
-            var eachproperty = Math.round(parseInt(cuts_property[i])*Reinforce1*ranproperty)
-            if (result_property === 'null') {
-              var result_property = eachproperty + " ( " + random +" % "+ ")"
-            } else {
-              var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
-            }
-          }
-
-        } else if (parseInt(cuts_property[i]) < 0 ){
-          if (Reinforce == "checked") {
-            var Reinforce1 = Math.pow(1.07,5);
-          } else {
-            var Reinforce1 = 1;
-          }
-
-          if (random == 0) {
-            var eachproperty = parseInt(cuts_property[i]);
-            if (result_property === 'null') {
-              var result_property = eachproperty + " ( " + random +" % "+ ")"
-            } else {
-              var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
-            }
-          } else if ( random > 0 ){
-            var ranproperty = (100 + random )/100
-            var eachproperty = Math.round(parseInt(cuts_property[i])*Reinforce1*ranproperty)
-            if (result_property === 'null') {
-              var result_property = eachproperty + " ( " + random +" % "+ ")"
-            } else {
-              var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
-            }
-          } else {
-            var ranproperty = (100 + random )/100
-            var eachproperty = Math.round(parseInt(cuts_property[i])*Reinforce1*ranproperty)
-            if (result_property === 'null') {
-              var result_property = eachproperty + " ( " + random +" % "+ ")"
-            } else {
-              var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
-            }
-          }
-        }
-      }
 
 
 
@@ -402,6 +313,101 @@ router.get('/ing', (req,res,next) => {
         var result_second_stats =second_stat + " ( " + Dice_roll_second_stat + " % " + ") ";
       }
       var result_stats = result_first_stats +" / " +result_second_stats;
+
+
+
+      //속방 재조립
+      var result_property = 'null'
+      var Allproperty = response.rows[0].armproperty;
+
+      var cut1_property = Allproperty.indexOf("(")
+      var cut2_property = Allproperty.indexOf(")")
+      var cut_property = Allproperty.substring(cut1_property+1,cut2_property)
+      var cuts_property = cut_property.split("/")
+
+
+      for (var i = 0; i < cuts_property.length; i++) {
+        if (Special === "checked") {
+          var random = Dice_roll(-20,40)
+        } else {
+          var random = Dice_roll(-30,30)
+        }
+
+        if (parseInt(cuts_property[i]) == 0) {
+          var eachproperty = 0;
+          if (result_property === 'null') {
+            var result_property = eachproperty
+          } else {
+            var result_property = result_property + " / " + eachproperty
+          }
+        } else if (parseInt(cuts_property[i]) > 0 ) {
+          //속방 수치가 0 이상일때
+          if (Reinforce == "checked") {
+            var Reinforce1 = 1.8435;
+          } else {
+            var Reinforce1 = 1;
+          }
+          if (random == 0) {
+            var eachproperty = parseInt(cuts_property[i]);
+            if (result_property === 'null') {
+              var result_property = eachproperty + " ( " + random +" % "+ ")"
+            } else {
+              var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
+            }
+          } else if ( random > 0 ){
+            var ranproperty = (100 + random )/100
+            var eachproperty = Math.round(parseInt(cuts_property[i])*Reinforce1*ranproperty)
+            console.log(eachproperty)
+            if (result_property === 'null') {
+              var result_property = eachproperty + " ( " + random +" % "+ ")"
+            } else {
+              var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
+            }
+          } else {
+
+            var ranproperty = (100 + random )/100
+            var eachproperty = Math.round(parseInt(cuts_property[i])*Reinforce1*ranproperty)
+            if (result_property === 'null') {
+              var result_property = eachproperty + " ( " + random +" % "+ ")"
+            } else {
+              var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
+            }
+          }
+
+        } else if (parseInt(cuts_property[i]) < 0 ){
+          if (Reinforce == "checked") {
+            var Reinforce1 = Math.pow(1.07,5);
+          } else {
+            var Reinforce1 = 1;
+          }
+
+          if (random == 0) {
+            var eachproperty = parseInt(cuts_property[i]);
+            if (result_property === 'null') {
+              var result_property = eachproperty + " ( " + random +" % "+ ")"
+            } else {
+              var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
+            }
+          } else if ( random > 0 ){
+            var ranproperty = (100 + random )/100
+            var eachproperty = Math.round(parseInt(cuts_property[i])*Reinforce1*ranproperty)
+            if (result_property === 'null') {
+              var result_property = eachproperty + " ( " + random +" % "+ ")"
+            } else {
+              var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
+            }
+          } else {
+            var ranproperty = (100 + random )/100
+            var eachproperty = Math.round(parseInt(cuts_property[i])*Reinforce1*ranproperty)
+            if (result_property === 'null') {
+              var result_property = eachproperty + " ( " + random +" % "+ ")"
+            } else {
+              var result_property = result_property + " / " + eachproperty + " ( " + random +" % "+ ")"
+            }
+          }
+        }
+      }
+
 
       var data =
         {
