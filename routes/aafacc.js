25a6a56d06fb419301c797c8f99ab477d9d98b5d
플렝미 <forflame0@gmail.com>
2020-06-08 18:55:26 +0900

    no message

--- routes/aafacc.js
+++ routes/aafacc.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var sslRedirect = require('heroku-ssl-redirect');
 var app = express();
-app.use(sslRedirect());
 const { Client } = require('pg');
 
 const client = new Client({
