d6c687ca8e3388c328c75fee56f074e4b2e7005d
플렝미 <forflame0@gmail.com>
2020-06-27 15:25:57 +0900

    no message

--- routes/test.js
+++ routes/test.js
@@ -184,10 +184,28 @@ router.post('/fixwp', (req,res,next) => {
 router.get('/:id', (req,res,next) => {
   var SearchType = req.query.searchType;
   if (SearchType === 'name') {
-    console.log(req.query.searchText2)
+    console.log(req.query.searchText2);
+    console.log(req.query.searchType2);
+    console.log(req.query.searchText2.length)
     var Search = req.query.searchText;
     var CurrentPage = req.params.id;
-    var QueryString = "SELECT *, count(*) over() as totalcount FROM aquafeq.aquafwp where wpname Ilike $1 ORDER BY wplimit,wpid asc limit 10 offset (($2- 1)*10);"
+    if (req.query.searchText2 != undefined) {
+      var Search2 = req.query.searchText2;
+      var SearchType2 = req.query.searchType2;
+      var Searchcount = req.query.searchText2.length;
+      for (var i = 0; i < Searchcount; i++) {
+        var SearchPlus = 'AND SearchType2.[i] Ilike Search2.[i]'
+        SearchPlus = + SearchPlus
+      }
+      //var SearchPlus = 'AND SearchType2.[0] Ilike Search2.[0] AND SearchType2.[1] Ilike Search2.[1] AND SearchType2.[2] Ilike Search2.[2] '
+      console.log(SearchPlus)
+      //SELECT *, count(*) over() as totalcount FROM aquafeq.aquafwp WHERE wpname Ilike $1  ORDER BY wplimit,wpid asc limit 10 offset (($2- 1)*10)
+      var QueryString = "SELECT *, count(*) over() as totalcount FROM aquafeq.aquafwp where wpname Ilike $1" + SearchPlus + " ORDER BY wplimit,wpid asc limit 10 offset (($2- 1)*10);"
+    } else {
+      var QueryString = "SELECT *, count(*) over() as totalcount FROM aquafeq.aquafwp where wpname Ilike $1 ORDER BY wplimit,wpid asc limit 10 offset (($2- 1)*10);"
+
+    }
+    console.log(QueryString)
     client.query(QueryString, ['%' + Search + '%', CurrentPage], (err, response) => {
       if(typeof(response.rows[0]) !== "object") {
         var TotalCount = 1;
