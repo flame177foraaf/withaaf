3d376b55ba925230bfb8bf0bec018fee894985fb
플렝미 <forflame0@gmail.com>
2020-07-03 15:57:38 +0900

    no message

--- routes/test.js
+++ routes/test.js
@@ -78,29 +78,17 @@ router.post('/', (req, res, next) => {
 
 });
 
-router.post('/:id', (req, res, next) => {
+router.post('/comment', (req, res, next) => {
   var Comment_body = req.body.comment_box;
   var Comment_writer = req.body.comment_writer
-  var Fbid = req.params.id;
+  var Fbid = req.body.fbid;
 
-  comment_body = BoardBody.replace(/(?:\r\n|\r|\n)/g, '<br />');
+  Comment_body = Comment_body.replace(/(?:\r\n|\r|\n)/g, '<br />');
   var QueryString = "set timezone TO 'Asia/Seoul'";
   client.query(QueryString, (err,response) => {
     var QueryString = "INSERT INTO aquafeq.fb_comment(fbid, writer, body, time) values ($1, $2, $3, to_char(now(), 'YYYY-MM-DD HH24:MI'));"
     client.query(QueryString, [Fbid, Comment_writer, Comment_body], (err, response) => {
-      if (err) {
-        console.error();
-      } else {
-        var fbid = req.params.id;
-        client.query("SELECT fbid, fbtitle, fbbody, fbname, fbcreatedat FROM aquafeq.freeboard WHERE fbid=$1", [fbid], (err, response) => {
-          client.query("SELECT * FROM fb_comment WHERE fbid=$1", [fbid], (err, responsecomment) => {
-            res.render('testshowboard', {
-              data: response.rows[0],
-              data_comment: responsecomment.rows
-            });
-          })
-        });
-      };
+      res.send(response)
     });
   })
 });
