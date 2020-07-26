774cf384fde4272015b3968c81e7aabc589e88ad
플렝미 <forflame0@gmail.com>
2020-07-26 17:50:46 +0900

    no message

--- routes/test.js
+++ routes/test.js
@@ -29,7 +29,7 @@ router.get('/', (req,res,next) => {
 });
 //  var QueryString = "SELECT * FROM aquafeq.monster where mon_property Ilike $1;"
 //  var QueryString = "select * from aquafeq.field inner join aquafeq.monster on aquafeq.field.field_id =  aquafeq.monster.mon_field where aquafeq.monster.mon_property Ilike $1 order by aquafeq.field.field_id, aquafeq.monster.mon_lv;"
-// var QueryString = "select (ROW_NUMBER() over()) as num, (select count (DISTINCT field_id) from aquafeq.field  as t1 inner join aquafeq.monster as t2 on t1.field_id =  t2.mon_field where t2.mon_property Ilike $1), * from aquafeq.field  as t1 inner join aquafeq.monster as t2 on t1.field_id =  t2.mon_field where t2.mon_property Ilike $1 order by field_id, mon_lv;"
+// var QueryString = "select (ROW_NUMBER() over()) as num, (select count (DISTINCT field_id) from aquafeq.field  as table1 inner join aquafeq.monster as table2 on table1.field_id =  table2.mon_field where table2.mon_property Ilike $1), * from aquafeq.field  as table1 inner join aquafeq.monster as table2 on table1.field_id =  table2.mon_field where table2.mon_property Ilike $1 order by field_id, mon_lv;"
 
 // var QueryString = "SELECT * FROM aquafeq.monster where mon_name Ilike $1"
 
@@ -37,101 +37,71 @@ router.get('/', (req,res,next) => {
 
 
 router.get('/search', (req,res,next) => {
-  var SearchingType = req.query.SearchType;
-  var SearchingText = req.query.SearchText;
-  if (SearchingType === 'name'){
-    // var FieldNum = req.params.id;
-    var QueryString = "select (ROW_NUMBER() over()) as num, (select count (DISTINCT field_id) as searchcount from aquafeq.field  as t1 inner join aquafeq.monster as t2 on t1.field_id =  t2.mon_field  where t2.mon_name Ilike $1), * from aquafeq.field  as t1 inner join aquafeq.monster as t2 on t1.field_id =  t2.mon_field where t2.mon_name Ilike $1 order by field_id, mon_lv;"
-    client.query(QueryString, ['%' + SearchingText + '%'], (err,response) => {
-      console.log(SearchingText)
-      console.log(typeof(SearchingType))
-      console.log('데이터 길이' + response.rows.length)
-      var Data_length = response.rows.length;
-      console.log('첫 데이터' + response.rows[0])
-      if (typeof(response.rows[0]) !== 'object') {
-        res.redirect('/test')
+  var QueryString = "SELECT * FROM aquafeq.dungeon order by id asc"
+  client.query(QueryString, (err,response) => {
+    var QueryString = "SELECT * FROM aquafeq.dungeon_partition order by id"
+    client.query(QueryString, (err, response1) => {
+      var SearchingType = req.query.SearchType;
+      var SearchingText = req.query.SearchText;
+      if (SearchingType === 'name'){
+        // var FieldNum = req.params.id;
+        var QueryString = "select (ROW_NUMBER() over()) as num, * from aquafeq.dungeon_partition  as table1 inner join aquafeq.monster as table2 on table1.part =  table2.mon_field where table2.mon_name Ilike $1 order by table1.id, mon_lv;"
+        client.query(QueryString, ['%' + SearchingText + '%'], (err,response2) => {
+          var Data_length = response2.rows.length;
+          res.render('test', {
+            SearchingText:SearchingText,
+            title:'AAF 던전 몬스터 정보',
+            data:response.rows,
+            data_partition:response1.rows,
+            data_monster:response2.rows,
+            Data_length:Data_length,
+          });
+        });
+      } else  if (SearchingType === 'property'){
+        // var FieldNum = req.params.id;
+        var QueryString = "select (ROW_NUMBER() over()) as num, * from aquafeq.dungeon_partition  as table1 inner join aquafeq.monster as table2 on table1.part =  table2.mon_field where table2.mon_property Ilike $1 order by table1.id, mon_lv;;"
+        client.query(QueryString, ['%' + SearchingText + '%'], (err,response2) => {
+          var Data_length = response2.rows.length;
+          res.render('test', {
+            SearchingText:SearchingText,
+            title:'AAF 던전 몬스터 정보',
+            data:response.rows,
+            data_partition:response1.rows,
+            data_monster:response2.rows,
+            Data_length:Data_length,
+          });
+        });
+      } else if (SearchingType === 'type'){
+        var QueryString = "select (ROW_NUMBER() over()) as num, * from aquafeq.dungeon_partition  as table1 inner join aquafeq.monster as table2 on table1.part =  table2.mon_field where table2.mon_type Ilike $1 order by table1.id, mon_lv;"
+        client.query(QueryString, ['%' + SearchingText + '%'], (err,response) => {
+          var Data_length = response2.rows.length;
+          res.render('test', {
+            SearchingText:SearchingText,
+            title:'AAF 던전 몬스터 정보',
+            data:response.rows,
+            data_partition:response1.rows,
+            data_monster:response2.rows,
+            Data_length:Data_length,
+          });
+        });
 
+      } else if (SearchingType === 'collect') {
+        var QueryString = "select (ROW_NUMBER() over()) as num, * from aquafeq.dungeon_partition  as table1 inner join aquafeq.monster as table2 on table1.part =  table2.mon_field where table2.mon_common Ilike $1 or table2.mon_uncommon Ilike $1 or table2.mon_rare Ilike $1 order by table1.id,mon_lv;"
+        client.query(QueryString, ['%' + SearchingText + '%'], (err,response2) => {
+          var Data_length = response2.rows.length;
+          res.render('test', {
+            SearchingText:SearchingText,
+            title:'AAF 던전 몬스터 정보',
+            data:response.rows,
+            data_partition:response1.rows,
+            data_monster:response2.rows,
+            Data_length:Data_length,
+          });
+        });
       }
+    })
+  });
 
-      var Data_length = response.rows.length;
-        res.render('test', {
-        title:'AAF 던전 몬스터 정보',
-        data:response.rows,
-        SearchingText:SearchingText,
-        Data_length:Data_length,
-      });
-    });
-  } else  if (SearchingType === 'property'){
-    // var FieldNum = req.params.id;
-    var QueryString = "select (ROW_NUMBER() over()) as num, * from aquafeq.field  as t1 inner join aquafeq.monster as t2 on t1.field_id =  t2.mon_field where t2.mon_property Ilike $1 order by field_id, mon_lv;;"
-    client.query(QueryString, ['%' + SearchingText + '%'], (err,response) => {
-      console.log(SearchingText)
-      console.log(typeof(SearchingType))
-      console.log('데이터 길이' + response.rows.length)
-      var Data_length = response.rows.length;
-      console.log('첫 데이터' + response.rows[0])
-      if (typeof(response.rows[0]) !== 'object') {
-        res.redirect('/test')
-
-      }
-
-      var Data_length = response.rows.length;
-      res.render('test', {
-        title:'AAF 던전 몬스터 정보',
-        data:response.rows,
-        SearchingText:SearchingText,
-        Data_length:Data_length,
-
-      });
-    });
-  } else if (SearchingType === 'type'){
-    var QueryString = "select (ROW_NUMBER() over()) as num, * from aquafeq.field  as t1 inner join aquafeq.monster as t2 on t1.field_id =  t2.mon_field where t2.mon_type Ilike $1 order by field_id, mon_lv;;"
-    client.query(QueryString, ['%' + SearchingText + '%'], (err,response) => {
-      console.log(SearchingText)
-      console.log(typeof(SearchingType))
-      console.log('데이터 길이' + response.rows.length)
-      var Data_length = response.rows.length;
-      console.log('첫 데이터' + response.rows[0])
-      if (typeof(response.rows[0]) !== 'object') {
-        res.redirect('/test')
-
-      }
-
-      var Data_length = response.rows.length;
-
-      res.render('test', {
-        title:'AAF 던전 몬스터 정보',
-        data:response.rows,
-        SearchingText:SearchingText,
-        Data_length:Data_length,
-
-      });
-    });
-
-  } else if (SearchingType === 'collect') {
-    var QueryString = "select * from aquafeq.field as t1 inner join aquafeq.monster as t2 on t1.field_id = t2.mon_field where t2.mon_common Ilike $1 or t2.mon_uncommon Ilike $1 or t2.mon_rare Ilike $1 order by field_id,mon_lv;"
-    client.query(QueryString, ['%' + SearchingText + '%'], (err,response) => {
-      console.log(SearchingText)
-      console.log(typeof(SearchingType))
-      console.log('데이터 길이' + response.rows.length)
-      var Data_length = response.rows.length;
-      console.log('첫 데이터' + response.rows[0])
-      if (typeof(response.rows[0]) !== 'object') {
-        res.redirect('/test')
-
-      }
-
-      var Data_length = response.rows.length;
-
-      res.render('test', {
-        title:'AAF 던전 몬스터 정보',
-        data:response.rows,
-        SearchingText:SearchingText,
-        Data_length:Data_length,
-
-      });
-    });
-  }
 });
 
 // params.id 를 쓰는 라우터는 마지막에 쓰라고 한다
@@ -143,13 +113,8 @@ router.get('/:id', (req,res,next) => {
       var Field_Id = req.params.id;
       var QueryString = "SELECT * FROM aquafeq.monster as table1 left join aquafeq.dungeon_partition as table2 on table1.mon_field = table2.part where mon_field = $1 order by table1.mon_lv asc;"
       client.query(QueryString, [Field_Id], (err,response2) => {
-        console.log(req.params.id)
-        console.log(Field_Id)
-        console.log(QueryString)
-        console.log(response2.rows)
 
         var Data_length = response2.rows.length;
-        console.log(Data_length)
         if (Field_Id == "EarthTower" || Field_Id == "FireTower") {
           res.render('test_top', {
             title:'AAF 던전 몬스터 정보',
