60c9e94269ba9795bb43220ca84d85d63f270216
플렝미 <forflame0@gmail.com>
2020-11-06 18:23:17 +0900

    no message

--- routes/board.js
+++ routes/board.js
@@ -52,6 +52,12 @@ router.get('/write', (req, res, next) => {
 router.get('/:id', (req, res, next) => {
   var fbid = req.params.id;
   console.log(fbid)
+
+  console.log(typeof(fbid))
+  var fbid = parseInt(fbid)
+  console.log(fbid)
+  console.log(typeof(fbid))
+
   console.log(req.url)
   var QueryString = "SELECT * FROM aquafeq.freeboard WHERE fbid = $1"
   var QueryStringComment = "SELECT * FROM aquafeq.fb_comment WHERE fbid = $1"
