6ac361baaa6d68c775aafd36a3f67a57a6a3d57d
플렝미 <forflame0@gmail.com>
2020-12-20 14:03:47 +0900

    no message

--- routes/test.js
+++ routes/test.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
-var bodyParser = require('body-parser');
 var url = require('url');
-
 
 const { Client } = require('pg');
 
@@ -14,128 +12,142 @@ const client = new Client({
 
 client.connect();
 
-
-
-router.get('/', (req,res, next) => {
-  var psql = "SELECT * FROM aquafeq.freeboard ORDER BY fbid DESC"
-  client.query(psql, (err, response) => {
-    res.render('test', {
-      title: '자유게시판',
-      data: response.rows
-    });
-  });
-});
-
-/*
-router.get('/test/:page', (req,res, next) => {
-  var page = req.params.page
-  var totalcount ="SELECT count(*) from aquafeq.freeboard;"
-
-  var psql = "SELECT (row_number() OVER(ORDER BY fbid)) AS rownum, fbtitle, fbname, fbcreatedat, count(*) OVER() AS totalcount FROM aquafeq.freeboard ORDER BY fbid DESC limit 10 offset ($1 - 1) * 10";
-  client.query(psql, ['%' + page + '%'], (err, response) => {
-    res.render('boardtest', {
-      title: '자유게시판',
-      data: response.rows,
-
-
-    });
-  });
-});
-*/
-router.get('/write', (req, res, next) => {
-  res.render('boardwrite' ,{
-    title: '글 작성하기'
-  });
-});
-
-router.get('/:id', (req, res, next) => {
-  var fbid = req.params.id;
-  console.log(fbid)
-  console.log(typeof(fbid))
-  var fbid = parseInt(fbid)
-  console.log(fbid)
-  console.log(typeof(fbid))
-
-
-  var QueryString = "SELECT * FROM aquafeq.freeboard WHERE fbid = $1"
-  var QueryStringComment = "SELECT * FROM aquafeq.fb_comment WHERE fbid = $1"
-  client.query(QueryString, [fbid], (err, response) => {
-    console.log(QueryString)
-    if (err) {
-      console.log(err)
-      res.redirect('/test')
-    }
-
-    client.query(QueryStringComment, [fbid], (err, response_comment) => {
-      console.log(QueryStringComment)
-      if (err) {
-        console.log(err)
-        res.redirect('/test')
-      }
-      res.render('showboard', {
-        data: response.rows[0],
-        data_comment: response_comment.rows
-      });
-    });
-  });
-});
-
-
-
-router.post('/', (req, res, next) => {
-
-
-  var BoardBody = req.body.boardbody;
-  BoardBody = BoardBody.replace(/(?:\r\n|\r|\n)/g, '<br />');
-  var QueryString = "set timezone TO 'Asia/Seoul'";
-  var Commentcount = 0;
+router.get('/', (req,res,next) => {
+  var Data_length = 0;
+  var QueryString = "SELECT * FROM aquafeq.dungeon order by id asc"
   client.query(QueryString, (err,response) => {
-    var QueryString = "INSERT INTO aquafeq.freeboard(fbtitle, fbbody, fbname, fbcreatedat, commentcount) values ($1, $2, $3, to_char(now(), 'YYYY-MM-DD HH24:MI:SS'), $4);"
-    client.query(QueryString, [req.body.title, BoardBody, req.body.writer, Commentcount], (err, response) => {
-      if (err) {
-        console.log(err);
+    var QueryString = "SELECT * FROM aquafeq.dungeon_partition order by id asc"
+    client.query(QueryString, (err, response1) => {
+      res.render('test', {
+      title:'AAF 던전 몬스터 정보',
+      fieldname:'검색이 필요합니다',
+      data:response.rows,
+      data_partition:response1.rows,
+      Data_length:Data_length,
+      });
+    })
+  });
+});
+//  var QueryString = "SELECT * FROM aquafeq.monster where mon_property Ilike $1;"
+//  var QueryString = "select * from aquafeq.field inner join aquafeq.monster on aquafeq.field.field_id =  aquafeq.monster.mon_field where aquafeq.monster.mon_property Ilike $1 order by aquafeq.field.field_id, aquafeq.monster.mon_lv;"
+// var QueryString = "select (ROW_NUMBER() over()) as num, (select count (DISTINCT field_id) from aquafeq.field  as t1 inner join aquafeq.monster as t2 on t1.field_id =  t2.mon_field where t2.mon_property Ilike $1), * from aquafeq.field  as t1 inner join aquafeq.monster as t2 on t1.field_id =  t2.mon_field where t2.mon_property Ilike $1 order by field_id, mon_lv;"
+
+// var QueryString = "SELECT * FROM aquafeq.monster where mon_name Ilike $1"
+
+
+
+
+router.get('/search', (req,res,next) => {
+  var QueryString = "SELECT * FROM aquafeq.dungeon order by id asc"
+  client.query(QueryString, (err,response) => {
+    var QueryString = "SELECT  aquafeq.dungeon_partition.id, " + '"PartitionName"' + "," + '"FieldName"' + ",   part, COUNT(*) count   FROM aquafeq.dungeon_partition  inner join aquafeq.monster on aquafeq.monster.mon_field = aquafeq.dungeon_partition.part GROUP by  aquafeq.dungeon_partition.id, " + '"PartitionName"' + "," + '"FieldName"' + ",  part order by aquafeq.dungeon_partition.id;"
+
+
+    client.query(QueryString, (err, response1) => {
+      var SearchingType = req.query.searchtype;
+      console.log(SearchingType)
+      console.log(typeof(SearchingType))
+      var SearchingText = req.query.searchtext;
+      if (SearchingType === 'MonLvDown') {
+        var SearchingType2 = req.query.searchtype2;
+        var SearchingText2 = req.query.searchtext2;
+
+        var SearchingText = parseInt(SearchingText)
+        var SearchingText2 = parseInt(SearchingText2);
+        if (typeof(SearchingType2) !== 'undefined') {
+          var SearchingText2 = SearchingText2;
+        } else {
+          var SearchingText2 = 0;
+          var SearchingText2 = parseInt(SearchingText2);
+        }
+        //var QueryString = "select * from aquafeq.dungeon_partition as t1 inner join aquafeq.monster as t2 on t1.part = t2.mon_field where (t2.mon_lv - ($1::INTEGER)) % ($2::INTEGER) = 0::INTEGER;";
+        //console.log(QueryString)
+        //console.log(SearchingText)
+        //console.log(SearchingText2)
+        //console.log(typeof(SearchingText))
+        //console.log(typeof(SearchingText2))
+        //client.query(QueryString, [SearchingText2, SearchingText ], (err,response3) => {
+          console.log(QueryString)
+
+          var QueryString = "select * from aquafeq.dungeon_partition as t1 inner join aquafeq.monster as t2 on t1.part = t2.mon_field where (t2.mon_lv - ($1::INTEGER)) % ($2::INTEGER) = 0::INTEGER order by t1.id;";
+
+          client.query(QueryString, [SearchingText2, SearchingText], (err,response2) => {
+            if (err) {
+              console.log(err)
+            }
+            console.log(QueryString)
+            var Data_length = response2.rows.length;
+
+            res.render('test', {
+              Searching:'YES',
+              SearchingType:SearchingType,
+              title:'AAF 던전 몬스터 정보',
+              data:response.rows,
+              data_partition:response1.rows,
+
+              data_monster:response2.rows,
+              Data_length:Data_length,
+            });
+          });
+        //});
       } else {
-        var psql = "SELECT * FROM aquafeq.freeboard"
-        client.query(psql, (err, response) => {
-          res.redirect('/test')
+        if (SearchingType === 'name'){
+          var QueryString = "select * from aquafeq.dungeon_partition  as t1 inner join aquafeq.monster as t2 on t1.part =  t2.mon_field where t2.mon_name Ilike $1 order by t1.id, mon_lv;"
+        } else  if (SearchingType === 'property'){
+          var QueryString = "select * from aquafeq.dungeon_partition  as t1 inner join aquafeq.monster as t2 on t1.part =  t2.mon_field where t2.mon_property Ilike $1 NOT FieldName IN ('Training') order by t1.id, mon_lv;;"
+        } else if (SearchingType === 'type'){
+          var QueryString = "select * from aquafeq.dungeon_partition  as t1 inner join aquafeq.monster as t2 on t1.part =  t2.mon_field where t2.mon_type Ilike $1 order by t1.id, mon_lv;"
+        } else if (SearchingType === 'collect') {
+          var QueryString = "select * from aquafeq.dungeon_partition  as t1 inner join aquafeq.monster as t2 on t1.part =  t2.mon_field where t2.mon_common Ilike $1 or t2.mon_uncommon Ilike $1 or t2.mon_rare Ilike $1 order by t1.id,mon_lv;"
+        }
+        client.query(QueryString, ['%' + SearchingText + '%'], (err,response2) => {
+          if (err) {
+            console.log(err)
+          }
+          console.log(QueryString)
+
+          var Data_length = response2.rows.length;
+          res.render('test', {
+            Searching:'YES',
+            SearchingType:SearchingType,
+
+            title:'AAF 던전 몬스터 정보',
+            data:response.rows,
+            data_partition:response1.rows,
+            data_monster:response2.rows,
+            Data_length:Data_length,
+          });
         });
-      };
-    });
-  })
-
-});
-
-router.post('/comment', (req, res, next) => {
-
-  var Comment_body = req.body.comment_box;
-  var Comment_writer = req.body.comment_writer;
-  var Fbid =req.body.fbid;
-  var Count_Comment = req.body.commentcount;
-  if (Count_Comment == null) {
-    Count_Comment = 0;
-  }
-  Count_Comment = parseInt(Count_Comment)
-
-  var url ='/test/'+Fbid
-
-
-  Comment_body = Comment_body.replace(/(?:\r\n|\r|\n)/g, '<br />');
-  var QueryString = "set timezone TO 'Asia/Seoul'";
-  client.query(QueryString, (err,response) => {
-    if (err) {
-      console.log(err);
-    }
-    var QueryString = "INSERT INTO aquafeq.fb_comment(fbid, writer, body, time) values ($1, $2, $3, to_char(now(), 'YYYY-MM-DD HH24:MI'));"
-    client.query(QueryString, [Fbid, Comment_writer, Comment_body], (err, response) => {
-      if (err) {
-        console.log(err);
       }
-      Count_Comment = Count_Comment+1;
-      var QueryString = "UPDATE aquafeq.freeboard SET commentcount = $1 where fbid = $2"
-      client.query(QueryString, [Count_Comment, Fbid], (err, response) => {
-        res.redirect(url)
-      });
-    });
-  })
+    })
+  });
 });
+
+// params.id 를 쓰는 라우터는 마지막에 쓰라고 한다
+router.get('/:id', (req,res,next) => {
+  var QueryString = "SELECT * FROM aquafeq.dungeon order by id asc"
+  client.query(QueryString, (err,response) => {
+    var QueryString = "SELECT * FROM aquafeq.dungeon_partition order by id"
+    client.query(QueryString, (err, response1) => {
+      var Field_Id = req.params.id;
+      var QueryString = "SELECT * FROM aquafeq.monster as t1 left join aquafeq.dungeon_partition as t2 on t1.mon_field = t2.part where mon_field = $1 order by t1.mon_lv,id  asc;"
+      client.query(QueryString, [Field_Id], (err,response2) => {
+        console.log(response2.rows[0])
+        var Data_length = response2.rows.length;
+          res.render('test', {
+          Field_Id:Field_Id,
+          title:'AAF 던전 몬스터 정보',
+          fieldname:'검색이 필요합니다',
+          data:response.rows,
+          data_partition:response1.rows,
+          data_monster:response2.rows,
+          Data_length:Data_length,
+        });
+      });
+    })
+  });
+});
+
+
+
 module.exports = router;
