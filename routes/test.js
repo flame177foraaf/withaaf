29a2cda4a209d0a07733fed785acd5432be38bd1
플렝미 <forflame0@gmail.com>
2021-02-19 22:09:28 +0900

    no message

--- routes/test.js
+++ routes/test.js
@@ -21,7 +21,9 @@ router.get('/', async function(req, res, next) {
 
 router.get('/searchall',  async function  (req, res, next) {
   var Search = req.query.searchtext;
+  console.log(Search)
   var QueryString = "SELECT *, count(*) over() as totalcount FROM aquafeq.aquafrecipe WHERE collectname Ilike $1 OR collect1name Ilike $1 OR collect2name Ilike $1 OR collect3name Ilike $1 OR collect4name Ilike $1 OR collect5name Ilike $1 OR collect6name Ilike $1 ORDER BY recipenum asc;";
+  console.log(QueryString)
 
   var searchdataRecipe = [];
 
