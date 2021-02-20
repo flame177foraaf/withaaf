3414caea36407c387034fb0a9ebf9a5dcabf574b
플렝미 <forflame0@gmail.com>
2021-02-20 15:55:53 +0900

    no message

--- routes/search.js
+++ routes/search.js
@@ -32,25 +32,25 @@ router.get('/', async function(req, res, next) {
 
       var QueryString = " Select * From aquafeq.aquafwp WHERE wpup Ilike '%' || $1 || '%' ORDER BY wpid ;";
       if (Search.length == 1) {
-        var QueryString = " Select * From aquafeq.aquafwp WHERE wpup Ilke '%'수집품 || $1 || '%' ORDER BY wpid ;";
+        var QueryString = " Select * From aquafeq.aquafwp WHERE wpup Ilike '%'<br> || $1 || '%' ORDER BY wpid ;";
       }
       await client.query(QueryString, [Search], async function(err, dataWP) {
 
         var QueryString = " Select * From aquafeq.aquafarm WHERE armup Ilike '%' || $1 || '%' ORDER BY armid ;";
         if (Search.length == 1) {
-          var QueryString = " Select * From aquafeq.aquafarm WHERE armup Ilke '%'수집품 || $1 || '%' ORDER BY armid ;";
+          var QueryString = " Select * From aquafeq.aquafarm WHERE armup Ilike '%' <br> || $1 || '%' ORDER BY armid ;";
         }
         await client.query(QueryString, [Search], async function(err, dataARM) {
 
           var QueryString = " Select * From aquafeq.aquafacc WHERE accup Ilike '%' || $1 || '%' ORDER BY accid ;";
           if (Search.length == 1) {
-            var QueryString = " Select * From aquafeq.aquafacc WHERE accup Ilke '%'수집품 || $1 || '%' ORDER BY accid ;";
+            var QueryString = " Select * From aquafeq.aquafacc WHERE accup Ilike '%' <br> || $1 || '%' ORDER BY accid ;";
           }
           await client.query(QueryString, [Search], async function(err, dataACC) {
 
             var QueryString = "select * from aquafeq.monster where ( mon_common Ilike '%' || $1 || '%' or mon_uncommon Ilike '%' || $1 || '%' or mon_rare Ilike '%' || $1 || '%') AND mon_field not Ilike '%Tower%' order by mon_lv;";
             if (Search.length == 1) {
-              var QueryString = "select * from aquafeq.monster where ( mon_common = $1 or mon_uncommon = $1 or mon_rare = $1) AND mon_field not Ilike '%Tower%' order by mon_lv;";
+              var QueryString = "select * from aquafeq.monster where ( mon_common Ilike $1 or mon_uncommon = $1 or mon_rare = $1) AND mon_field not Ilike '%Tower%' order by mon_lv;";
             }
             await client.query(QueryString, [Search], async function(err, dataMonsterField) {
 
