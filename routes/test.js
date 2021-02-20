918f348c997aa6d1369846d18e2f0d51aa4374c5
플렝미 <forflame0@gmail.com>
2021-02-20 13:09:32 +0900

    no message

--- routes/test.js
+++ routes/test.js
@@ -16,59 +16,58 @@ client.connect();
 
 
 router.get('/', async function(req, res, next) {
-  res.render('test', {
-    title: '테스트'
-  });
-});
+  if (req.query.searchtext != undefined) {
+    res.render('test', {
+      title: '테스트'
+    });
+  } else {
+    var Search = req.query.searchtext;
+    console.log(Search)
+    var QueryString = "SELECT * FROM aquafeq.aquafrecipe WHERE collectname Ilike $1 OR collect1name Ilike $1 OR collect2name Ilike $1 OR collect3name Ilike $1 OR collect4name Ilike $1 OR collect5name Ilike $1 OR collect6name Ilike $1 ORDER BY recipenum asc;";
+    await client.query(QueryString, ['%' + Search + '%'], async function(err, dataRecipe) {
 
-router.get('/searchall', async function(req, res, next) {
-  var Search = req.query.searchtext;
-  console.log(Search)
-  var QueryString = "SELECT * FROM aquafeq.aquafrecipe WHERE collectname Ilike $1 OR collect1name Ilike $1 OR collect2name Ilike $1 OR collect3name Ilike $1 OR collect4name Ilike $1 OR collect5name Ilike $1 OR collect6name Ilike $1 ORDER BY recipenum asc;";
-  await client.query(QueryString, ['%' + Search + '%'], async function(err, dataRecipe) {
+      var QueryString = " Select * From aquafeq.aquafwp WHERE wpup Ilike $1 ORDER BY wpid ;";
+      await client.query(QueryString, ['%' + Search + '%'], async function(err, dataWP) {
 
-    var QueryString = " Select * From aquafeq.aquafwp WHERE wpup Ilike $1 ORDER BY wpid ;";
-    await client.query(QueryString, ['%' + Search + '%'], async function(err, dataWP) {
+        var QueryString = " Select * From aquafeq.aquafarm WHERE armup Ilike $1 ORDER BY armid ;";
+        await client.query(QueryString, ['%' + Search + '%'], async function(err, dataARM) {
 
-      var QueryString = " Select * From aquafeq.aquafarm WHERE armup Ilike $1 ORDER BY armid ;";
-      await client.query(QueryString, ['%' + Search + '%'], async function(err, dataARM) {
+          var QueryString = " Select * From aquafeq.aquafacc WHERE accup Ilike $1 ORDER BY accid ;";
+          await client.query(QueryString, ['%' + Search + '%'], async function(err, dataACC) {
 
-        var QueryString = " Select * From aquafeq.aquafacc WHERE accup Ilike $1 ORDER BY accid ;";
-        await client.query(QueryString, ['%' + Search + '%'], async function(err, dataACC) {
+            var QueryString = "select * from aquafeq.monster where mon_common Ilike $1 or mon_uncommon Ilike $1 or mon_rare Ilike $1 AND mon_field not Ilike '%Tower%' order by mon_lv;";
+            await client.query(QueryString, ['%' + Search + '%'], async function(err, dataMonsterField) {
 
-          var QueryString = "select * from aquafeq.monster where mon_common Ilike $1 or mon_uncommon Ilike $1 or mon_rare Ilike $1 AND mon_field not Ilike '%Tower%' order by mon_lv;";
-          await client.query(QueryString, ['%' + Search + '%'], async function(err, dataMonsterField) {
+              var QueryString = "select * from aquafeq.monster where mon_common Ilike $1 or mon_uncommon Ilike $1 or mon_rare Ilike $1  AND mon_field Ilike '%Tower%' order by mon_lv;";
+              await client.query(QueryString, ['%' + Search + '%'], async function(err, dataMonsterTower) {
 
-            var QueryString = "select * from aquafeq.monster where mon_common Ilike $1 or mon_uncommon Ilike $1 or mon_rare Ilike $1  AND mon_field Ilike '%Tower%' order by mon_lv;";
-            await client.query(QueryString, ['%' + Search + '%'], async function(err, dataMonsterTower) {
+                await dataMonsterTower;
 
-              await dataMonsterTower;
+                res.render('test', {
+                  title: 'AAF 장비',
+                  dataRecipe: dataRecipe.rows,
+                  dataWP: dataWP.rows,
+                  dataARM: dataARM.rows,
+                  dataACC: dataACC.rows,
+                  dataMonsterField: dataMonsterField.rows,
+                  dataMonsterTower: dataMonsterTower.rows
 
-              res.render('test', {
-                title: 'AAF 장비',
-                dataRecipe: dataRecipe.rows,
-                dataWP: dataWP.rows,
-                dataARM: dataARM.rows,
-                dataACC: dataACC.rows,
-                dataMonsterField: dataMonsterField.rows,
-                dataMonsterTower: dataMonsterTower.rows
-
+                });
               });
+
+
             });
 
 
           });
-
-
         });
+
       });
 
     });
-
-  });
-
-
+  }
 });
+
 
 
 
