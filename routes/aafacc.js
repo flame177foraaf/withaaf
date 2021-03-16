ae0c6cc74c89786d7c419d2b309585c0142a81b3
플렝미 <forflame0@gmail.com>
2021-03-16 22:32:48 +0900

    no message

--- routes/aafacc.js
+++ routes/aafacc.js
@@ -7,7 +7,7 @@ var router = asyncify(express.Router());
 const { Client } = require('pg');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  // ssl: true,
 });
 
 client.connect();
--- routes/search.js
+++ routes/search.js
@@ -9,7 +9,7 @@ var {
 } = require('pg');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  // ssl: true,
 });
 
 client.connect();
