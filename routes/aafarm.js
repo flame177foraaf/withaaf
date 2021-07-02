f22f28e3a34b92af8c9cf3822501150d8ae8d339
플렝미 <forflame0@gmail.com>
2021-07-02 19:17:34 +0900

    no message

--- routes/aafarm.js
+++ routes/aafarm.js
@@ -131,15 +131,15 @@ router.post('/fixarm', async function(req, res, next) {
   }
 
 
-  var QueryString = "UPDATE aquafeq.aquafarm SET (armgrade, armlimit, armsocket, armether, armstats, armproperty, armfeat, armcustom, armup, armname) = ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)  WHERE armid = $11 returning *"
+  var QueryString = "UPDATE aquafeq.aquafarm SET (armgrade, armlimit, armsocket, armether, armstats, armproperty, armfeat, armcustom, armup, armname) = ($1, " + Armlimit + "$2, $3, $4, $5, $6, $7, $8, $9)  WHERE armid = $10 returning *"
 
   if (Armlimit == '') {
     console.log('ㅁㄴㅇㅁㅇㄴ')
-    QueryString = "UPDATE aquafeq.aquafarm SET (armgrade, armsocket, armether, armstats, armproperty, armfeat, armcustom, armup, armname) = ($1,  $3, $4, $5, $6, $7, $8, $9, 10)  WHERE armid = $11  returning *"
+    QueryString = "UPDATE aquafeq.aquafarm SET (armgrade, armsocket, armether, armstats, armproperty, armfeat, armcustom, armup, armname) = ($1,  $3, $4, $5, $6, $7, $8, $9)  WHERE armid = $10 and arm returning *"
   }
   console.log(QueryString);
 
-  client.query(QueryString, [Armgrade, Armlimit, Armsocket, Armether, Armstats, Armproperty, Armfeat, Armcustom, Armup, Armname, Eqid], async function(err, response) {
+  client.query(QueryString, [Armgrade, Armsocket, Armether, Armstats, Armproperty, Armfeat, Armcustom, Armup, Armname, Eqid], async function(err, response) {
     console.log(QueryString);
     console.log(err)
     var QueryString = "select * from aquafeq.aquafarm where armname = $1"
